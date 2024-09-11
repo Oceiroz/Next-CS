421b1a637d8f88d8d28b427cc3bfb28ef1648208
Lucas <115151663+Oceiroz@users.noreply.github.com>
2024-09-11 12:35:12 +0100

    Add files via upload

---
+++ Next/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Next
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = GetIntInput("Please input a number to solve");
+            int[] valueList = ValueAsList(value);
+            string strValue = FormatList(valueList);
+            if (strValue == value.ToString())
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(strValue);
+            }
+        }
+        static int GetIntInput(string inputMessage)
+        {
+            int userInput = 0;
+            while (true)
+            {
+                Console.WriteLine($"{inputMessage}\n");
+                string rawInput = Console.ReadLine();
+                try
+                {
+                    userInput = int.Parse(rawInput);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not a valid input\n");
+                }
+            }
+            return userInput;
+        }
+        static int[] ValueAsList(int value)
+        {
+            string newValue = Convert.ToString(value);
+            int[] valueList = new int[newValue.Length];
+            int x = 0;
+            foreach (char character in newValue)
+            {
+                string newCharacter = character.ToString();
+                int newCharacter2 = Convert.ToInt32(newCharacter);
+                valueList[x] = newCharacter2;
+                x++;
+            }
+            return valueList;
+        }
+        static string FormatList(int[] valueList)
+        {
+            Array.Sort(valueList);
+            Array.Reverse(valueList);
+            string newValue = String.Join("", valueList);
+            return newValue;
+        }
+    }
+}
